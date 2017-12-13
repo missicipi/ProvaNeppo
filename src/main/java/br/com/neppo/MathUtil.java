d034d08af5639e45d8e89bd6bf8ba709d17954d8
Ulisses Messias Machado <professorulissesm@gmail.com>
2017-12-13 16:11:42 +0000

    MathUtil.java edited online with Bitbucket

--- src/main/java/br/com/neppo/MathUtil.java
+++ src/main/java/br/com/neppo/MathUtil.java
@@ -3,7 +3,7 @@ package br.com.neppo;
 public class MathUtil {
 
     /**
-     * Dado um conjunto de nÃºmeros inteiros "ints" e um nÃºmero arbitrÃ¡rio "sum",
+     * Dado um conjunto de números inteiros "ints" e um número arbitrário "sum",
      * retorne true caso exista pelo menos um subconjunto de "ints" cuja soma soma dos seus elementos
      * seja igual a "sum"
      *
@@ -13,7 +13,23 @@ public class MathUtil {
      * @throws IllegalArgumentException caso o argumento "ints" seja null
      */
     public static boolean subsetSumChecker(int ints[], int sum) throws Exception {
-        throw new UnsupportedOperationException("Please implement me.");
-    }
-
-}
+-        throw new UnsupportedOperationException("Please implement me.");
+-    }
++        if(ints == null){
++            throw new UnsupportedOperationException("OS VALORES SÃO NULOS");
++        }else{
++            if(ints==sum){
++                return true;;
++            }else{
++                total = 0;
++                for(int i : ints){
++                    total = total + i;
++                    if(total == sum){
++                        return true;
++                    }
+ 
+-}
++                }
++                return false;
++            }
++        }
